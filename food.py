41a30a2af92ceaeca1c56616ec4f206e2e76598f
Alpenglow42 <30480333+Alpenglow42@users.noreply.github.com>
2021-12-10 15:44:27 +0700

    Create food.py

---
+++ food.py
@@ -0,0 +1,30 @@
+import random
+
+from turtle import Turtle, Screen
+screen = Screen()
+screen.colormode(255)
+
+food_shapes = random.choice(["turtle", "square", "circle", "triangle"])
+food_color = random.choice(["blue", "green", "yellow", "red", "orange"])
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape(food_shapes)
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color(food_color)
+        self.speed("fastest")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.shape(food_shapes)
+        self.color(food_color)
+        self.goto(random_x, random_y)
+
