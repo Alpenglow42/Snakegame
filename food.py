02fe0d9ba45b5767555da941382ca0cfdecdd1a9
Alpenglow42 <30480333+Alpenglow42@users.noreply.github.com>
2021-12-11 10:54:19 +0700

    Update food.py

--- food.py
+++ food.py
@@ -4,18 +4,18 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.colormode(255)
 
-food_shapes = random.choice(["turtle", "square", "circle", "triangle"])
-food_color = random.choice(["blue", "green", "yellow", "red", "orange"])
+food_shapes = ["turtle", "square", "circle", "triangle"]
+food_color = ["blue", "green", "yellow", "red", "orange"]
 
 
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.shape(random.choice(["turtle", "square", "circle", "triangle"]))
+        self.shape(random.choice(food_shapes))
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color(random.choice(["blue", "green", "yellow", "red", "orange"]))
+        self.color(random.choice(food_color))
         self.speed("fastest")
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
@@ -24,7 +24,8 @@ class Food(Turtle):
     def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
-        self.shape(food_shapes)
-        self.color(food_color)
+        self.shape(random.choice(food_shapes))
+        self.color(random.choice(food_color))
         self.goto(random_x, random_y)
+
 
